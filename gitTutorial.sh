cd7cc4e0787491d43c121fd946fa99e25d46ca6f
Jakub <117046128+KLesiu@users.noreply.github.com>
2024-04-19 22:33:24 +0200

    Init and add images,.sh file

---
+++ gitTutorial.sh
@@ -0,0 +1,20 @@
+git commit
+git checkout -b nowy_branch
+git merge main
+git rebase main
+git checkout HEAD^
+git log
+git checkout HEAD~4
+git branch -f main HEAD~3
+git reset
+git revert
+git status
+git add .
+git cherry pick
+git rebase -i HEAD~3
+git describe
+git commit -m  "Jakis commit"
+git tag v2 C3
+git clone
+git fetch
+git push origin main
